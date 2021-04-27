4f0dbaf132c29449021d3c4ad8827777a822693b
Tom LaMarre <tlamarre91+github@gmail.com>
2021-04-27 12:42:21 -0500

    Remove firebase-config from gitignore
    see https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public/37484053#37484053

---
+++ shared/src/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    "apiKey": "AIzaSyCfa3PcA2KD67RiizO4Tc9LpdVOFoTj0MQ",
+    "authDomain": "dev-trail-311217.firebaseapp.com",
+    "projectId": "dev-trail-311217",
+    "storageBucket": "dev-trail-311217.appspot.com",
+    "messagingSenderId": "27746945762",
+    "appId": "1:27746945762:web:ba31edec016b94e4922cbb",
+    "measurementId": "G-0Z3W9Q1PSR"
+}
